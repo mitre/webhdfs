fec7533a048ab2e9c703f62cd9265e56c7a07447
Brendan Hogan <bhogan@mitre.org>
2017-03-07 14:50:26 -0500

    first version that does anything

--- R/namenode_utilities.R
+++ R/namenode_utilities.R
@@ -9,8 +9,17 @@
 #'
 #' @examples
 get_webhdfs_url <- function() {
+  nn <- get_name_node_url()
 
+  # pending change to config yaml
+  # port <- get_webhdfs_port()
+  port <- "50070"
 
+  # build webhdfs url
+  webhdfs_url <- paste0(nn, ".mitre.org:", port,
+                        "/webhdfs/v1")
+
+  return(webhdfs_url)
 }
 
 
