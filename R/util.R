9f4012d9e522ce4f9502828a3035e48f4f835bf9
Brendan Hogan <bhogan@mitre.org>
2017-03-08 01:08:43 -0500

    more general unpacking of nested json results

--- R/util.R
+++ R/util.R
@@ -15,13 +15,48 @@ format_return <- function(dat, return_type){
 
 #' Convert HDFS file timestamps to POSIX
 #'
-#' @param x Numeric, milliseconds since January 1, 1970 UTC
+#' @param x Numeric, milliseconds since January 1, 1970 UTC.  Will accept a
+#'   character that can be converted to numeric.
 #'
 #' @return POSIXct formatted timestamp
 #' @export
 #'
 #' @examples
 hdfs_timestamp_to_posix <- function(x) {
-  return(as.POSIXct(x/1000, tz="UTC", origin="1970-01-01", format="%s"))
+  return(as.POSIXct(as.numeric(x)/1000,
+                    tz="UTC", origin="1970-01-01", format="%s"))
 
+}
+
+
+
+#' Unlist an object to the innermost \code{data.frame}
+#'
+#' Some calls to the WebHDFS REST API return a single JSON object, and some
+#' return an array of JSON objects.  This is a general function that will return
+#' a \code{data.frame} based on the output of either type from
+#' \code{\link[jsonlite]{fromJSON}}
+#'
+#' This is similar to simply calling \code{\link{as.data.frame}} on a
+#' \code{list}, but without the nested column names that result.  For example,
+#' where \code{\link{as.data.frame}} could generate a column name of
+#' \code{FileStatuses.FileStatus.accessTime}, this function will result in a
+#' column name of simply \code{accessTime}.
+#'
+#' @param x Object returned from call to \code{\link[jsonlite]{fromJSON}},
+#'   typically a list
+#'
+#' @return \code{data.frame} containing the innermost \code{data.frame}-like
+#'   object within \code{x}
+#' @export
+#'
+#' @examples
+unlist_carefully <- function(x) {
+
+  # grab the innermost object that is either not a list, or has length greater than 1
+  while (inherits(x, "list") & (length(x)==1)) {
+    x <- x[[1]]
+  }
+
+  return(as.data.frame(x))
 }
--- R/webhdfs_operation.R
+++ R/webhdfs_operation.R
@@ -13,6 +13,10 @@
 #' @return \code{data.frame} (or other requested type) containing output from
 #'   the WebHDFS operation
 #' @export
+#' @seealso
+#'   \url{http://hadoop.apache.org/docs/stable/hadoop-project-dist/hadoop-hdfs/WebHDFS.html}
+#'   for documentation of WebHDFS REST API commands that can be passed to the
+#'   \code{operation} parameter
 #'
 #' @examples
 #' hdfs_get("/data/raw/tma/SCHEDL", "LISTSTATUS")
@@ -21,6 +25,8 @@ hdfs_get <- function(path, operation, return_type=get_return_type()) {
 
   hdfs_path <- paste0(get_webhdfs_url(), path, "?op=", toupper(operation))
 
-  dat <- fromJSON(getURL(hdfs_path))[[1]][[1]]
+  dat_list <- fromJSON(getURL(hdfs_path))
+  dat <- unlist_carefully(dat_list)
+
   format_return(dat, return_type)
 }
