30e55c4261a56b8a9d23880c6ab799e5474018bf
Brendan Hogan <bhogan@mitre.org>
2017-03-09 15:51:13 -0500

    optionally recursively search for files within sub-directories from hdfs_ls

--- R/ls.R
+++ R/ls.R
@@ -5,6 +5,8 @@
 #' Shortcut for WebHDFS LISTSTATUS operation
 #'
 #' @param path Character containing file system path
+#' @param resursive Logical indicator of whether to resursively list individual
+#'   files within sub-directories.  Default FALSE.
 #' @param return_type character string. See \code{\link{set_return_type}} for
 #'   details and options.
 #'
@@ -15,17 +17,35 @@
 #' @examples
 #' hdfs_ls("/data/raw/tma/SCHEDL")
 #'
-hdfs_ls <- function(path, return_type=get_return_type()) {
+hdfs_ls <- function(path, recursive = FALSE, return_type=get_return_type()) {
 
-  # TODO: optionally recursively go through directories to get individual file listings
   # TODO: handle wildcards by getting the directory above it, everything from there, with the rest of the path below it
-
 
   dat <- hdfs_get(path, "LISTSTATUS", return_type=return_type)
 
   dat$modificationTime <- hdfs_timestamp_to_posix(dat$modificationTime)
   dat$accessTime <- hdfs_timestamp_to_posix(dat$accessTime)
 
-  return(dat)
+  if (recursive == TRUE) {
+    # split results into files and directories
+    dat_file <- dat[dat$type == "FILE"]
+    dat_dir <- dat[dat$type == "DIRECTORY"]
+
+    # for each sub-directory
+    for (sub_dir in dat_dir$pathSuffix) {
+      # search sub-directories for files
+      sub_path <- paste0(path, "/", sub_dir)
+      sub_result <- hdfs_ls(sub_path, recursive=recursive, return_type=return_type)
+
+      # combine files from sub-directory with files in existing list
+      dat_file <- rbind(dat_file, sub_result)
+    }
+
+    # return file list
+    return(dat_file)
+  } else {
+    return(dat)
+  }
+
 }
 
