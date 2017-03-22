8f509ff4fcaa854bd6ddf02ae18028c5936f4759
Brendan Hogan <bhogan@mitre.org>
2017-03-22 17:08:42 -0400

    set default user after resetting cache, and not only from onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -3,12 +3,12 @@
 .onLoad <- function(libname, pkgname) {
   assign("pkg_globals", new.env(), envir=parent.env(environment()))
   reset_cache()
-
-  set_default("webhdfs.user", guess_user(), "set_user")
 }
 
 reset_cache <- function() {
   rm(list=ls(envir=pkg_globals), envir=pkg_globals)
+
+  set_default("webhdfs.user", guess_user(), "set_user")
 }
 
 get_var <- function(varstr) {
