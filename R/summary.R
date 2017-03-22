cd23971fe3425dd8b03eec5168ceb5e22d648ca9
Brendan Hogan <bhogan@mitre.org>
2017-03-22 17:19:17 -0400

    [CGRP-87] add user parameter to hdfs_summary

--- R/summary.R
+++ R/summary.R
@@ -5,6 +5,9 @@
 #' Shortcut for WebHDFS GETCONTENTSUMMARY operation
 #'
 #' @param path Character containing file system path
+#' @param user Character username to use in WebHDFS operation.  If not provided,
+#'   \code{webhdfs.user} will be used and if that has not been set, a call to
+#'   \code{\link{guess_user}} will be made.
 #' @param return_type character string. See \code{\link{set_return_type}} for
 #'   details and options.
 #'
@@ -17,9 +20,9 @@
 #' hdfs_sumary("/data/blah/")
 #' }
 #'
-hdfs_summary <- function(path, return_type=get_return_type()) {
+hdfs_summary <- function(path, user = get_user(), return_type=get_return_type()) {
 
-  dat <- hdfs_get(path, "GETCONTENTSUMMARY", return_type=return_type)
+  dat <- hdfs_get(path, "GETCONTENTSUMMARY", user = user, return_type=return_type)
 
   return(dat)
 }
